3edda51cda7b95e6d61193ec72142c7ec1323c9d
adampalecki <114879329+adampalecki@users.noreply.github.com>
2023-03-27 13:45:56 +0200

    Add files via upload

---
+++ app-files/data.js
@@ -0,0 +1,94 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dji_fly_20230116_160752_130_1673883531133_pano",
+      "name": "dji_fly_20230116_160752_130_1673883531133_pano",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.3508879978886199,
+        "pitch": 0.04997211256995371,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.34340599034861086,
+          "pitch": 0.3896826136820124,
+          "rotation": 3.141592653589793,
+          "target": "1-dji_fly_20230312_104112_152_1678615174848_pano"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6365457024029215,
+          "pitch": -0.05697570650037953,
+          "title": "Wrocław",
+          "text": "111"
+        }
+      ]
+    },
+    {
+      "id": "1-dji_fly_20230312_104112_152_1678615174848_pano",
+      "name": "dji_fly_20230312_104112_152_1678615174848_pano",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.1449779605388759,
+        "pitch": 0.04390789586848243,
+        "fov": 0.7173138692231004
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.3851402330932583,
+          "pitch": 0.6259400588330877,
+          "title": "Niwnica 150",
+          "text": "Oto domek"
+        }
+      ]
+    }
+  ],
+  "name": "Próba",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
